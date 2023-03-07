a0287a24f02413f727fc2bbed4a1b3e3fb3c3eb1
adj-w-hp <adjyez@yahoo.com>
2023-03-07 23:36:36 +0300

    add: bindIP 127.0.0.1

--- bin/www.js
+++ bin/www.js
@@ -12,7 +12,7 @@ const debug = debugLib('express-simple-starter:server')
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '5000')
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 /**
  * Create HTTP server.
